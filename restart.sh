c3b000c3c30ce036fd5d4554c5a6737b728ecb47
wanhebin <48253420+wanhebin@users.noreply.github.com>
2023-03-28 14:10:24 +0800

    Update restart.sh
    Adjust the script format

--- restart.sh
+++ restart.sh
@@ -72,7 +72,7 @@ fi
 Text5="服务启动成功！"
 Text6="服务启动失败！"
 if [[ $CpuArch =~ "x86_64" ]]; then
-  nohup $Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir &> $Log_Dir/clash.log &
+	nohup $Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir &> $Log_Dir/clash.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 elif [[ $CpuArch =~ "aarch64" ||  $CpuArch =~ "arm64" ]]; then
