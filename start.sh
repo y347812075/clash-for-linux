fa6e99cbd81e91e59cd691eb3a1b3edaefaff0f7
wanhebin <48253420+wanhebin@users.noreply.github.com>
2023-06-26 23:32:24 +0800

    Update start.sh
    增强代理相关变量的适配性。部分Linux发行版会使用大写的变量  #95  。

--- start.sh
+++ start.sh
@@ -89,6 +89,9 @@ fi
 unset http_proxy
 unset https_proxy
 unset no_proxy
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset NO_PROXY
 
 
 ## Clash 订阅地址检测及配置文件下载
@@ -187,6 +190,9 @@ function proxy_on() {
 	export http_proxy=http://127.0.0.1:7890
 	export https_proxy=http://127.0.0.1:7890
 	export no_proxy=127.0.0.1,localhost
+    	export HTTP_PROXY=http://127.0.0.1:7890
+    	export HTTPS_PROXY=http://127.0.0.1:7890
+ 	export NO_PROXY=127.0.0.1,localhost
 	echo -e "\033[32m[√] 已开启代理\033[0m"
 }
 
@@ -195,6 +201,9 @@ function proxy_off(){
 	unset http_proxy
 	unset https_proxy
 	unset no_proxy
+  	unset HTTP_PROXY
+	unset HTTPS_PROXY
+	unset NO_PROXY
 	echo -e "\033[31m[×] 已关闭代理\033[0m"
 }
 EOF
